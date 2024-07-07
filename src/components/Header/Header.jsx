10e1feba99a6d54a50fd7389c08bf0059c24ac8c
aromalkallingal <aromalkallingal5@gmail.com>
2024-07-07 23:47:42 +0530

    Shopping Website first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your furniture here</h2>
+        <p>Choose from a wide variety of furnitures made with utmost perfection to suit your home and office needs.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
